41f0c089e7eec0caf301df31197e74591c2193c4
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-04-18 10:49:45 +0300

    Category için servis ve metotları yazıldı. AutoMapper eşlemeleri yapıldı.

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Catalog.DTOs.CategoryDTOs;
+
+namespace MultiShop.Catalog.Services.CategoryServices;
+
+public interface ICategoryService
+{
+    Task<List<ResultCategoryDTO>> GetAllCategoryAsync();
+    Task CreateCategoryAsync(CreateCategoryDTO createCategoryDTO);
+    Task UpdateCategoryAsync(UpdateCategoryDTO updateCategoryDTO);
+    Task DeleteCategoryAsync(string id);
+    Task<GetByIdCategoryDTO> GetByIdCategoryAsync(string id);
+}
