60d29b09f8bdee6d82c7f1be7a421fcca44c53de
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-04-18 15:52:14 +0300

    Program.cs dosyasında IoC Container configuration'u yapıldı. Transient denilen şey aslında bir nevi Prototype tasarım şablonudur. Her istek geldiğinde istek başına bir tane nesne üretilir. Her seferinde yeni nesne üretildiği için performansı olumsuz etkileyebilir. İstek tamamlanmadan başka bir istek gelirse yenisi yaratılır eskisi verilmez!!! Transient ile Scoped farkı budur.
    Scoped denilen şey aslında bir nevi Prototype tasarım şablonudur. Her istek geldiğinde istek başına bir tane nesne üretilir. Her seferinde yeni nesne üretildiği için performansı olumsuz etkileyebilir. İstek tamamlanmadan başka bir istek gelirse yenisi yaratılmadan zaten var olan verilir.
    
    Singleton bir tasarım şablonudur. Bir nesneden sadece bir tane olabilir bellekte. Her istek geldiğinde o nesne(aynı nesne) verilir. Program sonlanana kadar bellekte varlığını korur.

--- Services/Discount/MultiShop.Discount/Program.cs
+++ Services/Discount/MultiShop.Discount/Program.cs
@@ -1,6 +1,12 @@
+using MultiShop.Discount.Context;
+using MultiShop.Discount.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddTransient<DapperContext>();
+builder.Services.AddTransient<IDiscountService, DiscountService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
