be496582b90dfa0c3dabc6eaa66a0333db4802e2
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-04-18 13:36:32 +0300

    Discount(iskonto/indirim) microservice için DTO'lar yazıldı. Proje geliştikçe ihtiyaçlarda değişir bundan dolayı farklı DTO'lara ihtiyaç duyulur. DTO(Data Transfer Object) Entity yerine kullanılır. Veri alış verişi için Entity'leri kullanırsak güvenlik zafiyeti olur password gibi kolonlar taşınmak istenmeyebilir veya bazı kolonlar gizlenmesi gerekebilir. Gizlenmesi gerektiği durumda DTO'larda gizlenmek istenen kolonlar yer almaz. DTO'larda farklı tablolardan gelen JOIN'lenmiş verilerde olabilir. JOIN için farklı nesnelerdeki(Entity)'lerin farklı kolonları DTO'ya yansıtılabilir. DTO'ya Complex Type'de denmektedir. Entity'den DTO'ya - DTO'dan ise Entity'ye dönüşüm için elle eşlemek gerekir. Elle eşlemek zor ve zahmetli olduğu için .NET'de AutoMapper Java'da ise ObjectMapper kütüphaneleri kullanılır. Mapper'lar oldukça pratiktir.

---
+++ Services/Discount/MultiShop.Discount/DTOs/CreateDiscountCouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Discount.DTOs
+{
+    public class CreateDiscountCouponDTO
+    {
+        public string Code { get; set; }
+        public int Rate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ValidDate { get; set; }
+    }
+}
---
+++ Services/Discount/MultiShop.Discount/DTOs/GetByIdDiscountCouponDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Discount.DTOs
+{
+    public class GetByIdDiscountCouponDTO
+    {
+        public int CouponId { get; set; }
+        public string Code { get; set; }
+        public int Rate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ValidDate { get; set; }
+    }
+}
