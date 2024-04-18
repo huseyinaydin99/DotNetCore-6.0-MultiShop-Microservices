c803f63f8ebaec8c1d76fe292f398b08b945e900
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-04-18 11:11:01 +0300

    ProductDetail için servis ve metotları yazıldı. AutoMapper eşlemeleri yapıldı.

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductDetailServices/IProductDetailService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Catalog.DTOs.ProductDetailDTOs;
+
+namespace MultiShop.Catalog.Services.ProductDetailDetailServices;
+
+public interface IProductDetailService
+{
+    Task<List<ResultProductDetailDTO>> GettAllProductDetailAsync();
+    Task CreateProductDetailAsync(CreateProductDetailDTO createProductDetailDTO);
+    Task UpdateProductDetailAsync(UpdateProductDetailDTO updateProductDetailDTO);
+    Task DeleteProductDetailAsync(string id);
+    Task<GetByIdProductDetailDTO> GetByIdProductDetailAsync(string id);
+    Task<GetByIdProductDetailDTO> GetByProductIdProductDetailAsync(string id);
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductDetailServices/ProductDetailService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.DTOs.ProductDetailDTOs;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.ProductDetailDetailServices;
+
+public class ProductDetailService : IProductDetailService
+{
+    private readonly IMapper _mapper;
+    private readonly IMongoCollection<ProductDetail> _productDetailCollection;
+
+    public ProductDetailService(IMapper mapper, IDatabaseSettings _databaseSettings)
+    {
+        var client = new MongoClient(_databaseSettings.ConnectionString);
+        var database = client.GetDatabase(_databaseSettings.DatabaseName);
+        _productDetailCollection = database.GetCollection<ProductDetail>(_databaseSettings.ProductDetailCollectionName);
+        _mapper = mapper;
+    }
+
+    public async Task CreateProductDetailAsync(CreateProductDetailDTO createProductDetailDTO)
+    {
+        var values = _mapper.Map<ProductDetail>(createProductDetailDTO);
+        await _productDetailCollection.InsertOneAsync(values);
+    }
+    public async Task DeleteProductDetailAsync(string id)
+    {
+        await _productDetailCollection.DeleteOneAsync(x => x.ProductDetailId == id);
+    }
+
+    public async Task<GetByIdProductDetailDTO> GetByIdProductDetailAsync(string id)
+    {
+        var values = await _productDetailCollection.Find<ProductDetail>(x => x.ProductDetailId == id).FirstOrDefaultAsync();
+        return _mapper.Map<GetByIdProductDetailDTO>(values);
+    }
+
+    public async Task<GetByIdProductDetailDTO> GetByProductIdProductDetailAsync(string id)
+    {
+        var values = await _productDetailCollection.Find<ProductDetail>(x => x.ProductId == id).FirstOrDefaultAsync();
+        return _mapper.Map<GetByIdProductDetailDTO>(values);
+    }
+
+    public async Task<List<ResultProductDetailDTO>> GettAllProductDetailAsync()
+    {
+        var values = await _productDetailCollection.Find(x => true).ToListAsync();
+        return _mapper.Map<List<ResultProductDetailDTO>>(values);
+    }
+    public async Task UpdateProductDetailAsync(UpdateProductDetailDTO updateProductDetailDTO)
+    {
+        var values = _mapper.Map<ProductDetail>(updateProductDetailDTO);
+        await _productDetailCollection.FindOneAndReplaceAsync(x => x.ProductDetailId == updateProductDetailDTO.ProductDetailId, values);
+    }
+}
