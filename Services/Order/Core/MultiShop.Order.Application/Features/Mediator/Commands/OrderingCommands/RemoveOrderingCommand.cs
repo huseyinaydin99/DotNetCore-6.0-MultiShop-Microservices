9b8ba29fe855a1edb5ae691f228754b2f07373d8
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-04-20 15:47:53 +0300

    Ordering Entity için CQRS - Mediator klasörleme yapısı ve tasarım kalıpları yazıldı. Sorgular, komutlar, sorgu sonuçları ve komut - sorgu işleyicileri yazıldı.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Commands/OrderingCommands/RemoveOrderingCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+
+public class RemoveOrderingCommand : IRequest
+{
+    public int Id { get; set; }
+
+    public RemoveOrderingCommand(int id)
+    {
+        Id = id;
+    }
+}
