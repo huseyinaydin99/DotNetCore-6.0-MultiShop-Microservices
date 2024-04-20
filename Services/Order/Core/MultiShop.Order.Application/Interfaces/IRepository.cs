e1ae3ab4bfa62afade8907da0e1fb1295586c040
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-04-20 11:23:49 +0300

    Order(sipariş) microservice Repository tasarım şablonu için Interfaces klasörüne IRepository yazıldı.

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using MultiShop.Order.Domain.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Interfaces;
+
+public interface IRepository<T> where T : Entity<int>, new()
+{
+    Task<List<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T Entity);
+    Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
+}
