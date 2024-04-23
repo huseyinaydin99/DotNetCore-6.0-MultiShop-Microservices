8dcdba67c961c62ebaf5e9a656093af900867ae6
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-04-23 21:52:58 +0300

    Cargo Microservice için DTO'lar ve WebAPI/Controllers yazıldı. Cargo Companies, Cargo Details, Cargo Operations.

---
+++ Services/Cargo/MultiShop.Cargo.DTO/Dtos/CargoDetailDtos/CreateCargoDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.Dto.Dtos.CargoDetailDtos;
+
+public class CreateCargoDetailDto
+{
+    public string SenderCustomer { get; set; }
+    public string ReceiverCustomer { get; set; }
+    public int Barcode { get; set; }
+    public int CargoCompanyId { get; set; }
+}
---
+++ Services/Cargo/MultiShop.Cargo.DTO/Dtos/CargoOperationDtos/CreateCargoOperationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.Dto.Dtos.CargoOperationDtos;
+
+public class CreateCargoOperationDto
+{
+    public string Barcode { get; set; }
+    public string Description { get; set; }
+    public DateTime OperationDate { get; set; }
+}
