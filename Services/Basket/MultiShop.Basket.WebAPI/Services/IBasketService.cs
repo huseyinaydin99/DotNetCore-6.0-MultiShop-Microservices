5bed12688a0161668a1752a72a5f68f1fe8f257b
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-04-24 13:56:28 +0300

    Redis üzerinden sepet CRUD operasyonları için IBasketService interface ve BasketService class yazıldı.

---
+++ Services/Basket/MultiShop.Basket.WebAPI/Services/IBasketService.cs
@@ -0,0 +1,10 @@
+﻿using MultiShop.Basket.Dtos;
+
+namespace MultiShop.Basket.Services;
+
+public interface IBasketService
+{
+    Task<BasketTotalDto> GetBasket(string userId);
+    Task SaveBasket(BasketTotalDto basketTotalDto);
+    Task DeleteBasket(string userId);
+}
