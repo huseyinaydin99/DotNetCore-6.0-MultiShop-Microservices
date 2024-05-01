82e1765dbbdc21565d774cb05f7072dbb064b4d7
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-01 20:30:57 +0300

    Docker/Portainer üzerinde CommentDb adında MSSQLServer/Linux Container deploy/up/canlıya taşıma edildi. CommentContext üzerinden MSSQLServer'a 1442 portu üzerinden bağlanıldı. Migration komutları eklendi ve Migration işlemi yapıldı.

---
+++ Services/Comment/MultiShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Context;
+
+public class CommentContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server = localhost,1442; Initial Catalog = MultiShopCommentDb; User = SA; Password=123456789Aa*;");
+    }
+
+    public DbSet<UserComment> UserComments { get; set; }
+}
