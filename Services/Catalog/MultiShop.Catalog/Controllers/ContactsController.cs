6c180937e0c08c4472c91248b813511d97b15ce6
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-03 12:57:35 +0300

    MultiShop.Catalog Microservice için WebAPI/Controller yazıldı. Program.cs'de IoC Container Configuration yapıldı.

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ContactsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.DTOs.ContactDTOs;
+using MultiShop.Catalog.Services.ContactServices;
+
+namespace MultiShop.Catalog.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class ContactsController : ControllerBase
+{
+    private readonly IContactService _ContactService;
+
+    public ContactsController(IContactService ContactService)
+    {
+        _ContactService = ContactService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ContactList()
+    {
+        var values = await _ContactService.GettAllContactAsync();
+        return Ok(values);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetContactById(string id)
+    {
+        var values = await _ContactService.GetByIdContactAsync(id);
+        return Ok(values);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateContact(CreateContactDTO createContactDTO)
+    {
+        await _ContactService.CreateContactAsync(createContactDTO);
+        return Ok("Mesaj başarıyla eklendi");
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteContact(string id)
+    {
+        await _ContactService.DeleteContactAsync(id);
+        return Ok("Mesaj başarıyla silindi");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateContact(UpdateContactDTO updateContactDTO)
+    {
+        await _ContactService.UpdateContactAsync(updateContactDTO);
+        return Ok("Mesaj başarıyla güncellendi");
+    }
+}
