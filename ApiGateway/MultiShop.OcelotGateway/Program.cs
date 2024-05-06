6c89219307a7581953d26e17df3af09d2d54a90b
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-06 14:34:06 +0300

    MultiShop.Ocelot APIGateway Microservice'de Ocelot.json dosyasının Configuration'a dahil olabilmesi için Program.cs dosyasında ayarlamalar yapıldı.

--- ApiGateway/MultiShop.OcelotGateway/Program.cs
+++ ApiGateway/MultiShop.OcelotGateway/Program.cs
@@ -1,6 +1,16 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+builder.Services.AddOcelot(configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
