06092404dd84a44c43f062702f88deee6896ff0d
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-06 15:10:09 +0300

    Ocelot APIGateway için JWTBearer kütüphanesi kuruldu. appsettings.json dosyasına IdentityServer'a ulaşabilmek için ilgili URL verildi. Program.cs dosyasında Config ile JWTBearer ile koruma altına alındı.

--- ApiGateway/MultiShop.OcelotGateway/Program.cs
+++ ApiGateway/MultiShop.OcelotGateway/Program.cs
@@ -2,9 +2,14 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication().AddJwtBearer("OcelotAuthenticationScheme", opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "ResourceOcelot";
+    opt.RequireHttpsMetadata = false;
+});
 
-
-IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("Ocelot.json").Build();
 builder.Services.AddOcelot(configuration);
 
 var app = builder.Build();
