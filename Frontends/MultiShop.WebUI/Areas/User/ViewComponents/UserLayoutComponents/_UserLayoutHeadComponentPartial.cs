da1d8645abd1b2c7014e1ac986b78422f0518b5a
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-12 19:09:55 +0300

    UI Layout için _UserLayoutHeadComponentPartial yazıldı.

---
+++ Frontends/MultiShop.WebUI/Areas/User/ViewComponents/UserLayoutComponents/_UserLayoutHeadComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.User.ViewComponents.UserLayoutViewComponents;
+
+public class _UserLayoutHeadComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
