6415ec2661f05cc12c528f6e51ec9dbdbc1ead1c
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-14 18:16:12 +0300

    Frontend kısmında IMessageService isimli interface'i implemente eden mesaj CRUD operasyonlarını yöneten MessageService sınıfı yazıldı. User Area için kullanıcının kendi gelen veya giden mesajlarını görebilmesi için MessageController yazıldı. View'ler üzerinde değişiklik yapıldı.

---
+++ Frontends/MultiShop.WebUI/Services/MessageServices/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Dto.MessageDtos;
+
+namespace MultiShop.WebUI.Services.MessageServices;
+
+public interface IMessageService
+{
+    Task<List<ResultInboxMessageDto>> GetInboxMessageAsync(string id);
+    Task<List<ResultSendboxMessageDto>> GetSendboxMessageAsync(string id);
+    //Task CreateMessageAsync(CreateMessageDto createMessageDto);
+    //Task UpdateMessageAsync(UpdateMessageDto updateMessageDto);
+    //Task DeleteMessageAsync(int id);
+    //Task<GetByIdMessageDto> GetByIdMessageAsync(int id);
+}
