d95b534bb0123e52902c0c7857288ba573b472ff
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-21 13:51:44 +0300

    MultiShop.SignalRRealTime Microservice'de CORS Configuration, servisler için IoC Configuration ve SignalRHub Configuration Program.cs'de yapıldı.

--- Services/SignalRRealTime/SignalRReamTimeApi/Program.cs
+++ Services/SignalRRealTime/SignalRReamTimeApi/Program.cs
@@ -1,6 +1,26 @@
+using MultiShop.SignalRRealTimeApi.Hubs;
+using MultiShop.SignalRRealTimeApi.Services.SignalRCommentServices;
+using MultiShop.SignalRRealTimeApi.Services.SignalRMessageServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+               .AllowAnyMethod()
+               .SetIsOriginAllowed((host) => true)
+               .AllowCredentials();
+    });
+});
+
+builder.Services.AddSignalR();
+
+builder.Services.AddScoped<ISignalRMessageService, SignalRMessageService>();
+builder.Services.AddScoped<ISignalRCommentService, SignalRCommentService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,10 +36,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
