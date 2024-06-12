7ada873cefc169ab4591e299a90e05e0a7ffb18d
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-06-12 20:12:34 +0300

    MultiShop.RabbitMQMessageApi Microservice ve MultiShop.RapidApi projeleri oluşturuldu. RabbitMQ projesine RabbitMQClient paketi kuruldu.

---
+++ RapidApi/MultiShop.RapidApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultiShop.RapidApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Services/RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace MultiShop.RabbitMQMessageApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MessageController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult CreateMessage()
+    {
+        var connectionFactory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+        var connection = connectionFactory.CreateConnection();
+        var channel = connection.CreateModel();
+        return Ok("Mesajınız kuyruğa alınmıştır.");
+    }
+}
