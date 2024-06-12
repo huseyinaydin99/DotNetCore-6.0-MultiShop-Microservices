19b202486a0394ffb4f8b8855f27a42414d01590
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-06-12 20:48:06 +0300

    RabbitMQ Produce ve Consume kısmı yapıldı.

--- Services/RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessageController.cs
+++ Services/RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessageController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
 namespace MultiShop.RabbitMQMessageApi.Controllers;
 
@@ -17,6 +19,30 @@ public class MessageController : ControllerBase
         };
         var connection = connectionFactory.CreateConnection();
         var channel = connection.CreateModel();
+        channel.QueueDeclare("Kuyruk1", false, false, false, arguments: null);
+        var messageContent = "Merhaba bu bir RabbitMQ kuyruk mesajıdır. Selamun Aleyküm dayı...";
+        var byteMessageContent = Encoding.UTF8.GetBytes(messageContent);
+        channel.BasicPublish(exchange: "", routingKey: "Kuyruk1", basicProperties: null, body: byteMessageContent);
         return Ok("Mesajınız kuyruğa alınmıştır.");
+    }
+
+    [HttpGet]
+    public IActionResult ReadMessage()
+    {
+        var connectionFactory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+        var connection = connectionFactory.CreateConnection();
+        var channel = connection.CreateModel();
+        var consumer = new EventingBasicConsumer(channel);
+        var message = "";
+        consumer.Received += (model, x) =>
+        {
+            var byteMessage = x.Body.ToArray();
+            message = Encoding.UTF8.GetString(byteMessage);
+        };
+        channel.BasicConsume(queue: "Kuyruk1", autoAck: false, consumer: consumer);
+        return Ok(message);
     }
 }
